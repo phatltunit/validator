5e613259bb4c578820c312393c5269892c6fee9b
PhatLT <phatlt@unit.com.vn>
2026-02-02 17:58:25 +0700

    add comment to explain the problem

--- src/main/java/vn/com/phat/validator/field/Validator.java
+++ src/main/java/vn/com/phat/validator/field/Validator.java
@@ -5,6 +5,10 @@ import vn.com.phat.validator.context.ValidationContext;
 
 import java.util.List;
 
+/**
+ * To avoid the performance costs of autoboxing, I implemented specialized interfaces for primitive types
+ *
+ */
 public interface Validator<E> {
     List<ValidationError> validate(E data, ValidationContext<?,?> context);
 }
